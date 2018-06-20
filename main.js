7fff3de850b0c4a54c1698cf3033278cfe3ab594
VladKozlenko <vladkozlenko14@gmail.com>
2018-06-20 19:35:05 +0300

    Step 3
    Post

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 window.onload = init;
-var postUrl = 'http://localhost:3000/students';
+var StudentsUrl = 'http://localhost:3000/students';
 function init() {
-    loadPosts().then(renderPost);
+    loadStudents().then(renderStudents);
+    var form = document.getElementById("addNew");
+    form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            AddNewStudent()
+                .then(loadStudents)
+                .then(renderStudents);
+        })
 }
-function renderPost(post){
-    let postElement = document.getElementById('template-student');
-    let templateContent = postElement.content.getElementById('post');
+function renderStudents(students){
+    let studentElement = document.getElementById('template-student');
+    let templateContent = studentElement.content.getElementById('post');
     let studentsList = document.getElementById('students');
-    for (let st of post) {
-        let postClone = templateContent.cloneNode(true);
-        updatePostElement(postClone, st);
-        studentsList.appendChild(postClone);
+    studentsList.innerHTML = '';
+    for (let st of students) {
+        let studentClone = templateContent.cloneNode(true);
+        updateStudentElement(studentClone, st);
+        studentsList.appendChild(studentClone);
     }
 }
 
-function loadPosts(){
-    return fetch(postUrl).then(response => response.json());
+function loadStudents(){
+    return fetch(StudentsUrl).then(response => response.json());
 }
 
-function updatePostElement(element, st) {
+function AddNewStudent(){
+let stName = document.getElementById("studentName").value;
+let stKnowledge = document.getElementById("studentKnowledge").value;
+return fetch(StudentsUrl, {
+    method: "POST",
+    body: JSON.stringify({name: stName,knowledge: stKnowledge}),
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+}
+
+function updateStudentElement(element, st) {
     element.querySelector('h1').innerText = st.name;
-    element.querySelector('p').innerText = st.knowledge.toString();
+    element.querySelector('p').innerText = st.knowledge;
 }
