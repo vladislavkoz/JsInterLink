7d77cf15f27190df7ad681d9883433a24ecd072a
VladKozlenko <vladkozlenko14@gmail.com>
2018-06-20 18:59:14 +0300

    Second part with Get

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 window.onload = init;
-
+var postUrl = 'http://localhost:3000/students';
 function init() {
-    var student1 = {name: "student1", knowledge: 4};
-    var student2 = {name: "student2", knowledge: 12};
-    var student3 = {name: "student3", knowledge: 5};
-    var students = [student1, student2, student3];
+    loadPosts().then(renderPost);
+}
+function renderPost(post){
     let postElement = document.getElementById('template-student');
-    let post = postElement.content.getElementById('post');
+    let templateContent = postElement.content.getElementById('post');
     let studentsList = document.getElementById('students');
-    for (let st of students) {
-        let postClone = post.cloneNode(true);
+    for (let st of post) {
+        let postClone = templateContent.cloneNode(true);
         updatePostElement(postClone, st);
         studentsList.appendChild(postClone);
     }
+}
+
+function loadPosts(){
+    return fetch(postUrl).then(response => response.json());
 }
 
 function updatePostElement(element, st) {
